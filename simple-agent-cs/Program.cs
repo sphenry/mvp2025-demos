dfa72158a93aaae49727ddcd8d2cc000929a36b7
Shawn Henry <sphenry@gmail.com>
2025-03-25 15:12:48 -0700

    Initial checkin

---
+++ simple-agent-cs/Program.cs
@@ -0,0 +1,39 @@
+﻿using DotNetEnv;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+Env.Load();
+
+// Create the kernel
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(
+                Env.GetString("AZURE_OPENAI_DEPLOYMENT"),
+                Env.GetString("AZURE_OPENAI_ENDPOINT"),
+                Env.GetString("AZURE_OPENAI_API_KEY")
+                );
+var kernel = builder.Build();   
+
+string AgentName = "MySemanticKernelAgent";
+string AgentInstructions = "You are a helpful assistant. Be concise.";
+
+// Create the agent
+ChatCompletionAgent agent =
+        new()
+        {
+            Name = AgentName,
+            Instructions = AgentInstructions,
+            Kernel = kernel,
+        };
+
+// Create a message and a thread
+var input = "Why is the sky blue?";
+ChatMessageContent message = new(AuthorRole.User, input);
+
+AgentThread? thread = null;
+
+// Invoke the agent
+await foreach (AgentResponseItem<ChatMessageContent> response in agent.InvokeAsync(message, thread))
+{
+    Console.WriteLine($"Response: {response.Message}");
+    thread = response.Thread;
+}
