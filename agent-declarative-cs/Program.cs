4b6991a2f129e91dee89a46d0f4e7776ef42cfdc
Shawn Henry <sphenry@gmail.com>
2025-03-26 13:28:56 -0700

    added mcp sample

---
+++ agent-declarative-cs/Program.cs
@@ -0,0 +1,43 @@
+﻿using DotNetEnv;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+Env.Load();
+
+// Create the kernel
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(
+                Env.GetString("AZURE_OPENAI_DEPLOYMENT"),
+                Env.GetString("AZURE_OPENAI_ENDPOINT"),
+                Env.GetString("AZURE_OPENAI_API_KEY")
+                );
+var kernel = builder.Build();   
+
+KernelPlugin plugin = KernelPluginFactory.CreateFromType<MenuPlugin>();
+kernel.Plugins.Add(plugin);
+
+var text =
+    """
+    type: chat_completion_agent
+    name: FunctionCallingAgent
+    instructions: Use the provided functions to answer questions about the menu.
+    description: This agent uses the provided functions to answer questions about the menu.
+    model:
+        options:
+        temperature: 0.4
+    tools:
+        - id: MenuPlugin.GetSpecials
+        type: function
+        - id: MenuPlugin.GetItemPrice
+        type: function
+    """;
+var kernelAgentFactory = new ChatCompletionAgentFactory();
+
+var agent = await kernelAgentFactory.CreateAgentFromYamlAsync(text, kernel);
+
+await foreach (ChatMessageContent response in agent!.InvokeAsync(
+    new ChatMessageContent(AuthorRole.User, "What is the special soup and how much does it cost?")
+    ))
+{
+    this.WriteAgentChatMessage(response);
+}
---
+++ mcp-cs/Program.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using DotNetEnv;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol.Client;
+Env.Load();
+
+var apiKey = Env.GetString("OPENAI_API_KEY");
+var modelId = Env.GetString("OPENAI_CHAT_MODEL_ID");
+
+// Create an MCPClient for the GitHub server
+await using var mcpClient = await McpClientFactory.CreateAsync(
+    new()
+    {
+        Id = "github",
+        Name = "GitHub",
+        TransportType = "stdio",
+        TransportOptions = new Dictionary<string, string>
+        {
+            ["command"] = "npx",
+            ["arguments"] = "-y @modelcontextprotocol/server-github",
+        }
+    },
+    new() { ClientInfo = new() { Name = "GitHub", Version = "1.0.0" } }).ConfigureAwait(false);
+
+// Retrieve the list of tools available on the GitHub server
+var tools = await mcpClient.GetAIFunctionsAsync().ConfigureAwait(false);
+foreach (var tool in tools)
+{
+    Console.WriteLine($"{tool.Name}: {tool.Description}");
+}
+
+// Prepare and build kernel with the MCP tools as Kernel functions
+var builder = Kernel.CreateBuilder();
+builder.Services
+    .AddOpenAIChatCompletion(
+        serviceId: "openai",
+        modelId: modelId,
+        apiKey: apiKey);
+Kernel kernel = builder.Build();
+kernel.Plugins.AddFromFunctions("GitHub", tools.Select(aiFunction => aiFunction.AsKernelFunction()));
+
+// Enable automatic function calling
+OpenAIPromptExecutionSettings executionSettings = new()
+{
+    Temperature = 0,
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: new() { RetainArgumentTypes = true })
+};
+
+// Test using GitHub tools
+var prompt = "Summarize the last four commits to the microsoft/semantic-kernel repository?";
+var result = await kernel.InvokePromptAsync(prompt, new(executionSettings)).ConfigureAwait(false);
+Console.WriteLine($"\n\n{prompt}\n{result}");
